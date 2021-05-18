872d7d14203bc7f61c58e713fc241e6a89ad4d85
Nevallem <rogerlima@Outlook.com>
2021-05-18 13:34:29 -0300

    Update launcher.js

--- src/launcher.js
+++ src/launcher.js
@@ -1,14 +1,20 @@
-﻿/**
+/**
  * Creates and closes requests for adminship in pt.wikipedia
  *
  * @author [[w:pt:user:!Silent]]
  * @date 09/apr/2015
- * @update 24/oct/2017
+ * @update 18/mai/2021
  */
+/* jshint laxbreak: true */
 /* global mw */
 
 ( function () {
 'use strict';
+
+// Anonymous user
+if ( !mw.config.get( 'wgUserName' ) ) {
+	return;
+}
 
 if ( mw.config.get( 'wgPageName' ) === 'Wikipédia:Administradores/Pedidos_de_aprovação'
 	||  mw.config.get( 'wgPageName' ).indexOf( 'Wikipédia:Administradores/Pedidos_de_aprovação/' ) !== -1
